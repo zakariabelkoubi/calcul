b122087d360781d47212d52e7f42841547b438c0
zakariabelkoubi <opshanks422@gmail.com>
2022-09-09 18:05:05 +0100

    calcul

---
+++ main.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int main()
+{
+
+    float x,y,g;
+    char op;
+    //operator
+    //SIN-COS-TAN=====>
+    printf(" this parTe for sin/cos/tan\n");
+    printf("enter the angle:\n");
+    scanf("%f" , &g);
+    printf("the cos is: %.2f\n" , cos(g));
+
+    printf("the sin is:%.2f\n" , sin(g));
+
+    printf("the tan is: %.2f\n" , tan(g));
+    //=====<>=====>
+    printf("enter the first number:\n");
+    scanf("%d" , &x);
+    printf("enter the second number:\n");
+    scanf("%d" , &y);
+    printf("enter the operator:\n");
+
+}
